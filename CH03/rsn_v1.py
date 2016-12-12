45971bfafe44a33e4554e69a15a5573272a62297
root <root@localhost.localdomain>
2016-12-11 22:25:17 -0500

    completed rsn v1

--- CH03/rsn_v1.py
+++ CH03/rsn_v1.py
@@ -1,10 +1,50 @@
 #!/usr/bin/env python
-'''sniffer1'''
+'''sniffer'''
 
 import socket
 import os
+WINDOWS = 'nt'
+POSIX = 'posix'
 
-# host ip to listen on
-host = "192.168.124.128"
+def main():
+    '''sniffer'''
+    # host ip to listen on
+    host = "192.168.124.128"
 
-print os.name
+    os_name = os.name.lower()
+
+    protocols = {WINDOWS    : socket.IPPROTO_IP,
+                 POSIX    : socket.IPPROTO_ICMP}
+    try:
+        socket_protocol = protocols[os_name]
+    except:
+        print "No match for os.name. Add a protocol to [protocols] variable for %s" % os_name
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    print "Created sniffer"
+
+    sniffer.bind((host, 0))
+    print "Bound to host %s" % host
+
+    # we want the ip headers included in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    print "Headers enabled"
+
+    # if we are using windows we want to send and IOCTL to set up promiscuous mode
+    if os.name == WINDOWS:
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+        print "Enabled promiscuous mode"
+
+    try:
+        port = 65565
+        # read in a single packet
+        print "Waiting for data on port %d..." % port
+        print sniffer.recvfrom(port)
+    except:
+        pass
+    finally:
+        print "Cleaning up"
+        if os.name == WINDOWS:
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+            print "Disabled promiscuous mode"
+main()
