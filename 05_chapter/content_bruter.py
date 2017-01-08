91c047783d9f18ea15acf7930599c3617e6fe84e
root <root@localhost.localdomain>
2017-01-08 15:56:56 -0500

    untested content_bruter.py

--- 05_chapter/content_bruter.py
+++ 05_chapter/content_bruter.py
@@ -32,10 +32,14 @@ def getargs():
 
 def build_threads(thread_count, target, args):
     '''spawn the threads'''
+    threads = []
     for i in range(thread_count):
         print 'Spawning Thread: %d' % i
-        t = threading.Thread(target=target, args=args)
-        t.start()
+        threads.append(threading.Thread(target=target, args=args))
+    def starter():
+        for t in threads:
+            t.start()
+    return starter
 
 def build_queue(directory, filters):
     '''build the queue of paths'''
@@ -56,21 +60,90 @@ def build_queue(directory, filters):
 
     return result
 
+resume = None
+def build_wordqueue(wordlist_file):
+    '''build a queue of words from wordlist_file'''
+    # read the wordlist_file
+    fd = open(wordlist_file, 'rb')
+    raw_words = fd.readlines()
+    fd.close()
+
+    found_resume = False
+    words = Queue.Queue()
+
+    for word in raw_words:
+        word = word.rstrip()
+
+        if resume is not None:
+            if found_resume:
+                words.put(word)
+            else:
+                if word == resume:
+                    found_resume = True
+                    print 'Resuming wordlist from: %s' % resume
+        else:
+            words.put(word)
+
+    return words
+
+
+
+
+def dir_bruter(target_url, user_agent, word_queue, extensions=None):
+    '''brute force the target'''
+    while not word_queue.empty():
+        attempt = word_queue.get()
+
+        attempt_list = []
+        # check if we are looking at a file or folder
+
+        sformat = '/%s' # assume we are working with a file...
+        if '.' not in attempt:
+            # ... nope, it's a folder
+            sformat = sformat + '/'
+        attempt_list.append(sformat % attempt)
+
+        # if we want to brute force extensions
+        if extensions:
+            for extension in extensions:
+                attempt_list.append('/%s%s' % (attempt, extension))
+
+        # iterate over our list of attempts
+        for brute in attempt_list:
+            url = '%s%s' % (target_url, urllib.quote(brute))
+
+            try:
+                headers = {}
+                headers['User-Agent'] = user_agent
+
+                request = urllib2.Request(url, headers=headers)
+
+                response = urllib2.urlopen(request)
+
+                if len(response.read()):
+                    print '[%d] => %s' % (response.code, url)
+            except urllib2.URLError, err:
+                if hasattr(err, 'code') and err.code != 404:
+                    print '[!!!] [%d] => %s' % (err.code, url)
+
+    
 def main():
     '''main'''
     ns = getargs()
     print ns
 
     threads = ns.thread_count
-    target = ns.url
-    directory = ns.output_directory
-    filters = ns.filters
+    target_url = ns.url
+    wordlist_file = ns.wordlist
+    user_agent = ns.user_agent
+
+    extensions = ns.extensions
 
     # get the Queue
-    queue = build_path_queue(directory, filters)
+    queue = build_wordqueue(wordlist_file)
 
     # construct and start the threads
-    build_threads(threads, (queue, target))
+    build_threads(threads, dir_bruter, (target_url, user_agent, queue, extensions))
 
         
 main()
